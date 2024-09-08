cccc8da7727034af455a2215a89ca900c6495ff5
YtG12 <151299641+YtG12@users.noreply.github.com>
2024-09-08 04:43:21 -0700

    Add Plane Prototype Project

---
+++ Plane Prototype/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float propellerSpeed = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spins the plane’s propeller around the Z axis.
+        transform.Rotate(Vector3.forward * propellerSpeed * Time.deltaTime);
+    }
+}
